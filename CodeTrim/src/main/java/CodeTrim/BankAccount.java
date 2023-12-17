d7997e04b3d0e8664c1ecfca84f06cb6814be337
Jun Raymund Ruiz <105430839+juraara@users.noreply.github.com>
2023-12-17 17:29:01 +0800

    Update BankAccount.java

--- CodeTrim/src/main/java/CodeTrim/BankAccount.java
+++ CodeTrim/src/main/java/CodeTrim/BankAccount.java
@@ -1,67 +1,58 @@
-
 package CodeTrim;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-public class BankAccount
-{
-	// the attributes
-	private String accountNumber;
-	private String accountName;
-	protected double balance;
-        private String pin;
-	
-	// the methods
-	
-	// the constructor
-	public BankAccount(String numberIn, String nameIn)
-	{
-		accountNumber = numberIn;
-		accountName = nameIn;
-		balance = 0;
-                this.pin = pin;
-	}
-	
-	// methods to read the attributes
-	public String getAccountName()
-	{
-		return accountName;
-	}
-	
-	public String getAccountNumber()
-	{
-		return accountNumber;
-	}
-	
-	public double getBalance()
-	{
-		return balance;
-	}
-	   public String getPin() {
+public class BankAccount {
+    // the attributes
+
+    private String accountNumber;
+    private String accountName;
+    protected double balance;
+    private String pin;
+
+    // the methods
+    // the constructor
+    public BankAccount(String numberIn, String nameIn, String pin) {
+        accountNumber = numberIn;
+        accountName = nameIn;
+        balance = 0;
+        this.pin = pin;
+    }
+
+    // methods to read the attributes
+    public String getAccountName() {
+        return accountName;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public String getPin() {
         return pin;
     }
-	// methods to deposit and withdraw money
-	public void deposit(double amountIn)
-	{
-	balance = balance + amountIn;
-	}
-	public boolean withdraw(double amountIn)
-	{
-			if(amountIn > balance)
-			{
-				return false; // no withdrawal was made
-			}
-			else
-			{
-				balance = balance - amountIn;
-				return true; // money was withdrawn successfully
-			}
-	}
-         public boolean validateAccount(String enteredAccountNumber, String enteredPin) {
-        return accountNumber.equals(enteredAccountNumber) && pin.equals(enteredPin);   
+    // methods to deposit and withdraw money
+
+    public void deposit(double amountIn) {
+        balance = balance + amountIn;
+    }
+
+    public boolean withdraw(double amountIn) {
+        if (amountIn > balance) {
+            return false; // no withdrawal was made
+        } else {
+            balance = balance - amountIn;
+            return true; // money was withdrawn successfully
+        }
+    }
+
+    public boolean validateAccount(String enteredAccountNumber, String enteredPin) {
+        return accountNumber.equals(enteredAccountNumber) && pin.equals(enteredPin);
     }
 
 }
-
-
